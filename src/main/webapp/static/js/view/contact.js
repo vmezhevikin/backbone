64e8ce3a70a366eb42691dfaa6d4620dface4511
vmezhevikin <ve.mezhevikin@gmail.com>
2017-11-13 01:28:41 +0200

    added confirm delete dialog view

--- src/main/webapp/static/js/view/contact.js
+++ src/main/webapp/static/js/view/contact.js
@@ -1,5 +1,5 @@
-define(['jquery', 'underscore', 'backbone', 'text!template/contact.html'],
-    function ($, _, Backbone, contactTemplate) {
+define(['bootstrap', 'underscore', 'backbone', 'view/deleteModal', 'text!template/contact.html'],
+    function ($, _, Backbone, DeleteModal, contactTemplate) {
 
         return Backbone.View.extend({
 
@@ -8,16 +8,34 @@ define(['jquery', 'underscore', 'backbone', 'text!template/contact.html'],
             template: _.template(contactTemplate),
 
             events: {
-                'click .delete-button': 'deleteContact'
+                'click .delete-button': 'showModal'
+            },
+
+            initialize: function () {
+                this.render();
             },
 
             render: function () {
                 this.$el.html(this.template(this.model.attributes));
+                this.deleteModal = new DeleteModal({
+                    model: this.model
+                });
+                this.$el.append(this.deleteModal.$el);
+                this.listenTo(this.deleteModal, 'deletion-confirmed', this.deleteContact);
                 return this;
+            },
+
+            showModal: function () {
+                this.deleteModal.show();
+            },
+
+            hideModal: function () {
+                this.deleteModal.hide();
             },
 
             deleteContact: function () {
                 this.model.destroy();
+                this.deleteModal.remove();
                 this.remove();
             }
         });
---
+++ src/main/webapp/static/js/view/deleteModal.js
@@ -0,0 +1,38 @@
+define(['bootstrap', 'underscore', 'backbone', 'text!template/deleteModal.html'],
+    function ($, _, Backbone, deleteModalTemplate) {
+
+        return Backbone.View.extend({
+
+            template: _.template(deleteModalTemplate),
+
+            events: {
+                'click .confirm-delete-button': 'deleteContact'
+            },
+
+            initialize: function () {
+                this.render();
+            },
+
+            render: function () {
+                this.$el.html(this.template(this.model.attributes));
+                this.$modalEl = this.$el.children('.modal');
+                return this;
+            },
+
+            show: function () {
+                this.$modalEl.modal('show');
+            },
+
+            hide: function () {
+                this.$modalEl.modal('hide');
+            },
+
+            deleteContact: function () {
+                this.hide();
+                var that = this;
+                this.$modalEl.on('hidden.bs.modal', function () {
+                    that.trigger('deletion-confirmed');
+                });
+            }
+        });
+    });
